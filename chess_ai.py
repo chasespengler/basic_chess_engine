76dec19fcb9bde54293d7f47511546bc010daddd
chasespengler <chasespengler7@gmail.com>
2022-06-24 21:33:29 -0400

    min max multi toyed with

--- chess_ai.py
+++ chess_ai.py
@@ -52,14 +52,16 @@ def greedy_multi(gs, valid_moves):
     '''
     Algorithm to decide a move based on point advantage projected out several moves
     Bot Level 3
-    Really buggy currently. Weird rook or castling logic bug. Also weird nonetype move return.
     '''
     turn = 1 if gs.white_turn else -1
     max_score = -CM
     best_moves = []
     for move in valid_moves:
+        #Bot move 1
         gs.make_move(move)
+        #If move results in opponent checkmate then return move immediately
         if gs.white_checkmate or gs.black_checkmate:
+            #Undo bot move 1
             gs.undo_move()
             return move
         elif gs.stalemate:
@@ -67,43 +69,40 @@ def greedy_multi(gs, valid_moves):
         else:
             opponent_moves = gs.valid_moves()
             best_opp = greedy_1(gs, opponent_moves)
+            #Opponent move 1
             gs.make_move(best_opp)
-            print(gs.board)
-            print(gs.white_checkmate)
-            print(gs.white_in_check)
             if gs.white_checkmate or gs.black_checkmate or gs.stalemate:
+                #Undo opponent move
                 gs.undo_move()
+                #Undo bot move 1
                 gs.undo_move()
                 return greedy_1(gs, valid_moves)
             ahead_moves = gs.valid_moves()
-            if not ahead_moves:
-                print(gs.board)
-                gs.undo_move()
-                print(gs.board)
-                gs.undo_move()
-                print(gs.board)
             best_ahead_move = greedy_1(gs, ahead_moves)
-            print("Ahead ", best_ahead_move.piece_moved)
+            #Bot move 2
             gs.make_move(best_ahead_move)
             score = turn * material_score(gs.board)
+            #Undo bot move 2
             gs.undo_move()
+            #Undo opponent move
             gs.undo_move()
+        #Determining the score logic
         if score > max_score:
             best_moves = []
             max_score = score
             best_moves.append(move)
         elif score == max_score:
             best_moves.append(move)
+        #Undo bot move 1
         gs.undo_move()
 
     best_move = random_move(gs, best_moves)
-    print(best_moves)
-    print("Best Move ", best_move.piece_moved)
     return best_move
 
 def min_max_move(gs, valid_moves):
     '''
     Min max algorithm for bot play
+    Bot Level 4
     '''
     turn = 1 if gs.white_turn else -1
     opp_min_max_score = CM
@@ -111,11 +110,22 @@ def min_max_move(gs, valid_moves):
     opp_min_max_moves = []
     for move in valid_moves:
         gs.make_move(move)
+        if gs.white_checkmate or gs.black_checkmate:
+            return move
+        elif gs.stalemate:
+            #Make a stalemate move if alternative ends in checkmate? Maybe it doesn't need to be that smart
+            st_move = move
+            continue
         opp_moves = gs.valid_moves()
         opp_max_score = -CM
         for opp_move in opp_moves:
             gs.make_move(opp_move)
-            score = turn * material_score(gs.board)
+            if gs.white_checkmate or gs.black_checkmate:
+                score = CM
+            elif gs.stalemate:
+                score = SM
+            else:
+                score = -turn * material_score(gs.board)
             if score > opp_max_score:
                 opp_max_score = score
                 opp_best_move = opp_move
@@ -134,20 +144,68 @@ def min_max_move(gs, valid_moves):
 
     best_move = random_move(gs, best_moves)
     opp_best_move = random_move(gs, opp_min_max_moves)
-    return best_move, opp_best_move
+    return best_move
 
-def min_max_5(gs, valid_moves):
-    i = 30
-    for i in range(i):
-        move1, move2 = min_max_move(gs, valid_moves)
-        gs.make_move(move1)
-        gs.make_move(move2)
-    
-    for i in range(i):
-        gs.undo_move()
+def min_max_multi(gs, valid_moves):
+    turn = 1 if gs.white_turn else -1
+    opp_min_max_score = CM
+    best_moves = []
+    opp_min_max_moves = []
+    for move in valid_moves:
+        gs.make_move(move)
+        #Need stalemate logic
+        if gs.white_checkmate or gs.black_checkmate:
+            return move
+        elif gs.stalemate:
+            pass
+        opp_moves = gs.valid_moves()
+        opp_max_score = -CM
+        for opp_move in opp_moves:
+            gs.make_move(opp_move)
+            #Need to find a break logic here
+            if gs.white_checkmate or gs.black_checkmate:
+                pass
+            elif gs.stalemate:
+                pass
+            my_moves = gs.valid_moves()
+            for my_move in my_moves:
+                gs.make_move(my_move)
+                #Need stalemate logic
+                if gs.white_checkmate or gs.black_checkmate:
+                    return move
+                elif gs.stalemate:
+                    pass
+                new_opp_moves = gs.valid_moves()
+                for opp in new_opp_moves:
+                    gs.make_move(opp)
+                    if gs.white_checkmate or gs.black_checkmate:
+                        score = CM
+                    elif gs.stalemate:
+                        score = SM
+                    else:
+                        score = -turn * material_score(gs.board)
+                    if score > opp_max_score:
+                        opp_max_score = score
+                        opp_best_move = opp_move
+                        if opp_max_score < opp_min_max_score:
+                            best_moves = []
+                            opp_min_max_moves = []
+                            opp_min_max_score = opp_max_score
+                            opp_min_max_move = opp_best_move
+                            best_moves.append(move)
+                            opp_min_max_moves.append(opp_min_max_move)
+                        elif opp_max_score == opp_min_max_score:
+                            best_moves.append(move)
+                            opp_min_max_moves.append(opp_min_max_move)
+                    gs.undo_move()
+                gs.undo_move()
+            gs.undo_move()
         gs.undo_move()
 
-    return move1
+    best_move = random_move(gs, best_moves)
+    opp_best_move = random_move(gs, opp_min_max_moves)
+    return best_move
+        
 
 def material_score(board):
     '''
